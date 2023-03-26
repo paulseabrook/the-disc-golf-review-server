b991500741aa7dedad467395f2a18c23c1a37b3a
Paul Warren Seabrook <paulwarrenseabrook@gmail.com>
2023-03-26 16:52:34 -0400

    finish initial code to delete and update reviews

--- routes/review-routes.js
+++ routes/review-routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 // require disc model
 const Disc = require('../models/disc')
+// require Review Mode;
 // handle for 404 errors
 const { handle404 } = require('../lib/custom-errors')
 // token needed for signing in
@@ -19,16 +20,69 @@ router.post('/reviews', requireToken, (req, res, next) => {
   // assign review.user to the ObjectID of the user who send the request
   review.user = req.user._id
 
+  // find the disc by the id given
   Disc.findById(discId)
+    // call the handle404 function which throws an error if no record is found
     .then(handle404)
+    // then take the disc and push the review into the reviews sub doc
     .then((disc) => {
       disc.reviews.push(req.body.review)
 
+      // make sure to save the disc
       return disc.save()
     })
 
+    // if successful, give a 201 status
     .then((disc) => res.status(201).json({ disc: disc }))
     .catch(next)
 })
 
+// PATCH
+
+router.patch('/reviews/:id', requireToken, (req, res, next) => {
+  // from the body, the discID is the ObjectID of the disc we are reviewing
+  const discId = req.body.review.discId
+  Disc.findById(discId)
+    .then((disc) => {
+      if (disc.user.equals(req.user._id)) {
+        const review = disc.reviews.id(req.params.id)
+        review.comment = req.body.review.comment
+        review.rating = req.body.review.rating
+        return disc.save() // saves document with subdocuments and triggers validation
+      } else {
+        res.sendStatus(401)
+      }
+    })
+    .then((disc) => {
+      res.send({ disc })
+    })
+    .catch(next)
+})
+
+// DELETE
+
+router.delete('/reviews/:id', requireToken, (req, res, next) => {
+  // from the body, the discID is the ObjectID of the disc we are reviewing
+  const discId = req.body.review.discId
+  Disc.findById(discId)
+    .then((disc) => {
+      if (disc.user.equals(req.user._id)) {
+        disc.reviews.pull(req.params.id)
+        return disc.save()
+      } else {
+        res.sendStatus(401)
+      }
+    })
+    .then((disc) => {
+      res.send({ disc })
+    })
+    .catch(next)
+})
+
 module.exports = router
+
+// TODO
+
+// Ability to edit a comment and its rating
+
+// Ability to delete a comment and its rating
